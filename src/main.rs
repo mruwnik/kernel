1baaec937b1bdf2528771d6b81e7267011dac52d
Daniel O'Connell <github@ahiru.pl>
2023-07-14 21:39:10 +0200

    handle all tokens

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::fmt;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 enum SpecialLexeme {
@@ -7,9 +8,24 @@ enum SpecialLexeme {
     HashParam,
     DoubleQuote,
     Quote,
-    BackQoute,
+    BackQuote,
     Comma,
-    CommaAt,
+    Semicolon,
+}
+
+impl fmt::Display for SpecialLexeme {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", match self {
+            SpecialLexeme::LeftParam => "(",
+            SpecialLexeme::RightParam => ")",
+            SpecialLexeme::HashParam => "#(",
+            SpecialLexeme::DoubleQuote => "\"",
+            SpecialLexeme::Quote => "'",
+            SpecialLexeme::BackQuote => "`",
+            SpecialLexeme::Comma => ",",
+            SpecialLexeme::Semicolon => ";",
+        })
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -26,8 +42,12 @@ enum Lexeme {
     Token(String),
 }
 
+fn make_error<T>(err: String) -> Result<T, io::Error> {
+    Result::Err(io::Error::new(io::ErrorKind::InvalidData, err))
+}
+
 fn parse_str(chars: &mut std::str::Chars) -> Result<Lexeme, io::Error> {
-    let mut string = String::from("\"");
+    let mut string = format!("\"");
     let mut prev = ' ';
     for c in chars {
         string.push(c);
@@ -36,7 +56,15 @@ fn parse_str(chars: &mut std::str::Chars) -> Result<Lexeme, io::Error> {
         }
         prev = c;
     }
-    Result::Err(io::Error::new(io::ErrorKind::InvalidData, "Could not find end of string!"))
+    make_error(format!("Could not find end of string!"))
+}
+
+fn skip_to_line_end(chars: &mut std::str::Chars) -> () {
+    for c in chars {
+        if c == '\n' {
+            break;
+        }
+    }
 }
 
 fn tokenize(chars: &mut std::str::Chars) -> Result<Vec<Lexeme>, io::Error> {
@@ -46,12 +74,14 @@ fn tokenize(chars: &mut std::str::Chars) -> Result<Vec<Lexeme>, io::Error> {
     let mut current = String::new();
     while let Some(c) = chars.next() {
         let token: Token = match c {
+            '(' if prev == Token::Char('#') => Token::Special(SpecialLexeme::HashParam),
             '(' => Token::Special(SpecialLexeme::LeftParam),
             ')' => Token::Special(SpecialLexeme::RightParam),
             ',' => Token::Special(SpecialLexeme::Comma),
             '"' => Token::Special(SpecialLexeme::DoubleQuote),
-            '`' => Token::Special(SpecialLexeme::BackQoute),
+            '`' => Token::Special(SpecialLexeme::BackQuote),
             '\'' => Token::Special(SpecialLexeme::Quote),
+            ';' => Token::Special(SpecialLexeme::Semicolon),
             v => if v.is_whitespace() {
                 Token::WhiteSpace
             } else {
@@ -65,17 +95,27 @@ fn tokenize(chars: &mut std::str::Chars) -> Result<Vec<Lexeme>, io::Error> {
                 tokens.push(Lexeme::Token(current.clone()));
                 current.clear();
             },
+            Token::Special(
+                l @ (SpecialLexeme::Quote |
+                     SpecialLexeme::BackQuote |
+                     SpecialLexeme::Comma |
+                     SpecialLexeme::HashParam)
+            ) => return make_error(format!("Invalid token found: '{l}'")),
             Token::Special(l) => {
                 if let Token::Char(_) = prev {
                     tokens.push(Lexeme::Token(current.clone()));
                     current.clear();
                 }
-                tokens.push({
-                    match l {
-                        SpecialLexeme::DoubleQuote => parse_str(chars)?,
-                        _ => Lexeme::Special(*l)
-                    }
-                });
+                if SpecialLexeme::Semicolon == *l {
+                    skip_to_line_end(chars);
+                } else {
+                    tokens.push({
+                        match l {
+                            SpecialLexeme::DoubleQuote => parse_str(chars)?,
+                            _ => Lexeme::Special(*l)
+                        }
+                    });
+                }
             },
         }
         prev = token;
@@ -89,7 +129,7 @@ fn tokenize(chars: &mut std::str::Chars) -> Result<Vec<Lexeme>, io::Error> {
 
 
 fn main() {
-    let raw = String::from("(+ (- 1 2) \"asd\\\"asd\" 3 4)");
+    let raw = format!("(+ (- 1 2) \"asd\\\"asd\" 3 4)");
     let mut chars = raw.chars();
 
     println!("{raw}");
@@ -103,82 +143,93 @@ fn main() {
 mod tests {
     use yare::parameterized;
 
-    use crate::{Lexeme, SpecialLexeme, tokenize};
+    use crate::{Lexeme, SpecialLexeme, tokenize, make_error};
 
     #[parameterized(
-        empty = { String::from(""), vec![] },
-        empty_w_whitespace = { String::from("   \n \n   \t  "), vec![] },
-        symbol = { String::from("bla"), vec![Lexeme::Token(String::from("bla"))]  },
-        symbol_w_whitespace = { String::from("    bla\n   "), vec![Lexeme::Token(String::from("bla"))]  },
+        empty = { format!(""), vec![] },
+        empty_w_whitespace = { format!("   \n \n   \t  "), vec![] },
+        symbol = { format!("bla"), vec![Lexeme::Token(format!("bla"))]  },
+        symbol_w_whitespace = { format!("    bla\n   "), vec![Lexeme::Token(format!("bla"))]  },
 
-        single_string = { String::from("\"asd\""), vec![Lexeme::Token(String::from("\"asd\""))]  },
-        string_w_whitespace = { String::from(" \"a  sdn \n\"  "), vec![Lexeme::Token(String::from("\"a  sdn \n\""))]  },
-        escape_quote_string = { String::from("\"asd\\\"\""), vec![Lexeme::Token(String::from("\"asd\\\"\""))]  },
+        single_string = { format!("\"asd\""), vec![Lexeme::Token(format!("\"asd\""))]  },
+        string_w_whitespace = { format!(" \"a  sdn \n\"  "), vec![Lexeme::Token(format!("\"a  sdn \n\""))]  },
+        escape_quote_string = { format!("\"asd\\\"\""), vec![Lexeme::Token(format!("\"asd\\\"\""))]  },
 
         multi_string = {
-            String::from("  \"bla\" \"asd\""),
+            format!("  \"bla\" \"asd\""),
             vec![
-                Lexeme::Token(String::from("\"bla\"")),
-                Lexeme::Token(String::from("\"asd\"")),
+                Lexeme::Token(format!("\"bla\"")),
+                Lexeme::Token(format!("\"asd\"")),
             ]
         },
         multi_string_no_space = {
-            String::from("  \"bla\"\"asd\""),
+            format!("  \"bla\"\"asd\""),
             vec![
-                Lexeme::Token(String::from("\"bla\"")),
-                Lexeme::Token(String::from("\"asd\"")),
+                Lexeme::Token(format!("\"bla\"")),
+                Lexeme::Token(format!("\"asd\"")),
             ]
         },
 
         empty_list = {
-            String::from("  ()"),
+            format!("  ()"),
             vec![
                 Lexeme::Special(SpecialLexeme::LeftParam),
                 Lexeme::Special(SpecialLexeme::RightParam),
             ]
         },
         single_element_list = {
-            String::from(" (bla) "),
+            format!(" (bla) "),
             vec![
                 Lexeme::Special(SpecialLexeme::LeftParam),
-                Lexeme::Token(String::from("bla")),
+                Lexeme::Token(format!("bla")),
                 Lexeme::Special(SpecialLexeme::RightParam),
             ]
         },
         multi_element_list = {
-            String::from(" (bla ble wrar) "),
+            format!(" (bla ble wrar) "),
             vec![
                 Lexeme::Special(SpecialLexeme::LeftParam),
-                Lexeme::Token(String::from("bla")),
-                Lexeme::Token(String::from("ble")),
-                Lexeme::Token(String::from("wrar")),
+                Lexeme::Token(format!("bla")),
+                Lexeme::Token(format!("ble")),
+                Lexeme::Token(format!("wrar")),
                 Lexeme::Special(SpecialLexeme::RightParam),
             ]
         },
 
         nestedmulti_element_list = {
-            String::from(" (bla ble (+ 1 2) wrar) "),
+            format!(" (bla ble (+ 1 2) wrar) "),
             vec![
                 Lexeme::Special(SpecialLexeme::LeftParam),
-                Lexeme::Token(String::from("bla")),
-                Lexeme::Token(String::from("ble")),
+                Lexeme::Token(format!("bla")),
+                Lexeme::Token(format!("ble")),
                 Lexeme::Special(SpecialLexeme::LeftParam),
-                Lexeme::Token(String::from("+")),
-                Lexeme::Token(String::from("1")),
-                Lexeme::Token(String::from("2")),
+                Lexeme::Token(format!("+")),
+                Lexeme::Token(format!("1")),
+                Lexeme::Token(format!("2")),
                 Lexeme::Special(SpecialLexeme::RightParam),
-                Lexeme::Token(String::from("wrar")),
+                Lexeme::Token(format!("wrar")),
                 Lexeme::Special(SpecialLexeme::RightParam),
             ]
         },
 
-        special_token_list = {
-            String::from(" (, ' `) "),
+        multiline_expr = {
+            format!(" (bla\n   ble\n   wrar) "),
             vec![
                 Lexeme::Special(SpecialLexeme::LeftParam),
-                Lexeme::Special(SpecialLexeme::Comma),
-                Lexeme::Special(SpecialLexeme::Quote),
-                Lexeme::Special(SpecialLexeme::BackQoute),
+                Lexeme::Token(format!("bla")),
+                Lexeme::Token(format!("ble")),
+                Lexeme::Token(format!("wrar")),
+                Lexeme::Special(SpecialLexeme::RightParam),
+            ]
+        },
+
+        comments = {
+            format!(" (bla  ;; a nice comment to be ignored\n   ble; another comment\n   wrar) "),
+            vec![
+                Lexeme::Special(SpecialLexeme::LeftParam),
+                Lexeme::Token(format!("bla")),
+                Lexeme::Token(format!("ble")),
+                Lexeme::Token(format!("wrar")),
                 Lexeme::Special(SpecialLexeme::RightParam),
             ]
         },
@@ -190,11 +241,16 @@ mod tests {
     }
 
     #[parameterized(
-        non_terminated_string = { String::from("\"asdadsad") },
-        non_terminated_string2 = { String::from("bla bla \"asdadsad") },
+        non_terminated_string = { format!("\"asdadsad"), format!("Could not find end of string!") },
+        non_terminated_string2 = { format!("bla bla \"asdadsad"), format!("Could not find end of string!") },
+        invalid_char_quote = { format!("bla bla 'wrar ble ble"), format!("Invalid token found: '''") },
+        invalid_char_comma = { format!("bla bla , ble ble"), format!("Invalid token found: ','") },
+        invalid_char_backquote = { format!("bla bla `wrar ble ble"), format!("Invalid token found: '`'") },
+        invalid_char_hash_param = { format!("bla bla #(wrar) ble ble"), format!("Invalid token found: '#('") },
     )]
-    fn test_tokenize_error(input: String) {
+    fn test_tokenize_error(input: String, error: String) {
         let mut chars = input.chars();
-        assert!(tokenize(&mut chars).is_err());
+        let err = tokenize(&mut chars).unwrap_err();
+        assert_eq!(err.to_string(), error)
     }
 }
