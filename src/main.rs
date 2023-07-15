6943ea0aab6704b60115676f3ca9fb13d97ac0f8
Daniel O'Connell <github@ahiru.pl>
2023-07-16 01:07:52 +0200

    env hierarchies

--- src/main.rs
+++ src/main.rs
@@ -22,28 +22,6 @@ fn main() {
     // let lexeme = Lexeme::String(format!("+"));
     // dbg!(parse_token(&lexeme).expect("ad"));
 
-    dbg!(Value::Symbol(Symbol("asdad".to_string())));
-
-    let ground_env = Env::new(vec![]);
-
-    let env = Env::new(vec![ground_env.clone()]);
-
-    ground_env.borrow_mut().bind(
-        Symbol("asd".to_string()),
-        Value::Symbol(Symbol("val 1".to_string()))
-    );
-    ground_env.borrow_mut().bind(
-        Symbol("bla".to_string()),
-        Value::Symbol(Symbol("val 2".to_string()))
-    );
-    ground_env.borrow_mut().bind(
-        Symbol("ble".to_string()),
-        Value::Symbol(Symbol("val 3".to_string()))
-    );
-    env.borrow_mut().bind(
-        Symbol("fewwf".to_string()),
-        Value::Symbol(Symbol("val 5".to_string()))
-    );
-    dbg!(env);
+    crate::values::tester();
     // println!("{}", "123".parse().unwrap())
 }
--- src/values.rs
+++ src/values.rs
@@ -7,7 +7,7 @@ pub struct Symbol(pub String);
 
 #[derive(Debug, PartialEq)]
 pub struct Env {
-    bindings: HashMap<Symbol, Value>,
+    bindings: HashMap<Symbol, Rc<Value>>,
     parents: Vec<Rc<RefCell<Env>>>,
 }
 
@@ -22,19 +22,64 @@ impl Env {
     pub fn new(parents: Vec<Rc<RefCell<Env>>>) -> Rc<RefCell<Self>> {
         Rc::new(RefCell::new(Env { bindings: HashMap::new(), parents }))
     }
-    pub fn bind(&mut self, symbol: Symbol, value: Value) {
+    pub fn bind(&mut self, symbol: Symbol, value: Rc<Value>) {
         self.bindings.insert(symbol, value);
     }
-    pub fn get(&self, symbol: Symbol) -> Option<&Value> {
-        self.bindings.get(&symbol)
+    pub fn get(&self, symbol: Symbol) -> Option<Rc<Value>> {
+        if let Some(val) = self.bindings.get(&symbol) {
+            Some(val.clone())
+        } else {
+            for parent in self.parents.iter() {
+                if let Some(v) = parent.borrow().get(symbol.clone()) {
+                    return Some(v.clone());
+                };
+            }
+            None
+        }
     }
 }
+
+
+pub fn tester() {
+    let ground_env = Env::new(vec![]);
+
+    let parent1_1 = Env::new(vec![ground_env.clone()]);
+    let parent1_2 = Env::new(vec![parent1_1.clone()]);
+    let parent1_3 = Env::new(vec![parent1_2.clone()]);
+
+    let env = Env::new(vec![parent1_3.clone()]);
+
+    fn add(env: Rc<RefCell<Env>>, val: &str) {
+        env.borrow_mut().bind(
+            Symbol("key".to_string()),
+            Rc::new(Value::Symbol(Symbol(val.to_string())))
+        );
+    }
+    add(ground_env.clone(), "ground");
+    add(parent1_1.clone(), "parent 1 1");
+    // add(parent1_2.clone(), "parent 1 2");
+    // add(parent1_3.clone(), "parent 1 3");
+    // add(env.clone(), "env");
+
+    dbg!(env.borrow().get(Symbol("key".to_string())));
+    // dbg!(env);
+}
+
 
 #[cfg(test)]
 mod tests {
 
+    use std::rc::Rc;
+    use std::cell::RefCell;
     use std::collections::HashMap;
     use crate::values::{Value, Env, Symbol};
+
+    fn make_value(string: &str) -> Rc<Value> {
+        Rc::new(Value::Symbol(Symbol(string.to_string())))
+    }
+    fn add(env: &Rc<RefCell<Env>>, key: &str, val: &str) {
+        env.borrow_mut().bind(Symbol(key.to_string()), make_value(val));
+    }
 
     #[test]
     fn test_make_env_is_empty() {
@@ -46,17 +91,14 @@ mod tests {
     #[test]
     fn test_make_env_bind() {
         let env = Env::new(vec![]);
-        let key1 = Symbol("key 1".to_string());
-        let key2 = Symbol("key 2".to_string());
-        let key3 = Symbol("key 3".to_string());
-        env.borrow_mut().bind(key1.clone(), Value::Symbol(Symbol("value 1".to_string())));
-        env.borrow_mut().bind(key2.clone(), Value::Symbol(Symbol("value 2".to_string())));
-        env.borrow_mut().bind(key3.clone(), Value::Symbol(Symbol("value 3".to_string())));
+        add(&env, "key 1", "value 1");
+        add(&env, "key 2", "value 2");
+        add(&env, "key 3", "value 3");
 
         let mut map = HashMap::new();
-        map.insert(key1, Value::Symbol(Symbol("value 1".to_string())));
-        map.insert(key2, Value::Symbol(Symbol("value 2".to_string())));
-        map.insert(key3, Value::Symbol(Symbol("value 3".to_string())));
+        map.insert(Symbol("key 1".to_string()), make_value("value 1"));
+        map.insert(Symbol("key 2".to_string()), make_value("value 2"));
+        map.insert(Symbol("key 3".to_string()), make_value("value 3"));
 
         assert_eq!(env.borrow().bindings, map);
     }
@@ -65,29 +107,115 @@ mod tests {
     fn test_make_env_bind_replace() {
         let env = Env::new(vec![]);
         let key = Symbol("key".to_string());
-        env.borrow_mut().bind(key.clone(), Value::Symbol(Symbol("value 1".to_string())));
-        env.borrow_mut().bind(key.clone(), Value::Symbol(Symbol("value 2".to_string())));
-        env.borrow_mut().bind(key.clone(), Value::Symbol(Symbol("value 3".to_string())));
+        add(&env, "key", "value 1");
+        add(&env, "key", "value 2");
+        add(&env, "key", "value 3");
 
         let mut map = HashMap::new();
-        map.insert(key, Value::Symbol(Symbol("value 3".to_string())));
+        map.insert(key, make_value("value 3"));
 
         assert_eq!(env.borrow().bindings, map);
     }
 
     #[test]
-    fn test_make_env_get() {
+    fn test_make_env_basic_get() {
         let env = Env::new(vec![]);
-        let key1 = Symbol("key 1".to_string());
-        let key2 = Symbol("key 2".to_string());
-        let key3 = Symbol("key 3".to_string());
-        env.borrow_mut().bind(key1.clone(), Value::Symbol(Symbol("value 1".to_string())));
-        env.borrow_mut().bind(key2.clone(), Value::Symbol(Symbol("value 2".to_string())));
-        env.borrow_mut().bind(key3.clone(), Value::Symbol(Symbol("value 3".to_string())));
+        add(&env, "key 1", "value 1");
+        add(&env, "key 2", "value 2");
+        add(&env, "key 3", "value 3");
 
+        let key2 = Symbol("key 2".to_string());
         assert_eq!(
-            *env.borrow().get(key2.clone()).ok_or("asd").unwrap(),
-            Value::Symbol(Symbol("value 2".to_string()))
+            env.borrow().get(key2.clone()).ok_or("asd").unwrap(),
+            make_value("value 2")
         );
+    }
+
+    fn get_key(env: &Rc<RefCell<Env>>, key: &str) -> Rc<Value> {
+        env.borrow().get(Symbol(key.to_string())).unwrap()
+    }
+
+    #[test]
+    fn test_make_env_basic_get_vertical_parents() {
+        let ground_env = Env::new(vec![]);
+
+        let parent1_1 = Env::new(vec![ground_env.clone()]);
+        let parent1_2 = Env::new(vec![parent1_1.clone()]);
+        let parent1_3 = Env::new(vec![parent1_2.clone()]);
+
+        let env = Env::new(vec![parent1_3.clone()]);
+
+        add(&ground_env, "key", "ground");
+        assert_eq!(get_key(&env, "key"), make_value("ground"));
+
+        add(&parent1_1, "key", "parent 1 1");
+        assert_eq!(get_key(&env, "key"), make_value("parent 1 1"));
+
+        add(&parent1_2, "key", "parent 1 2");
+        assert_eq!(get_key(&env, "key"), make_value("parent 1 2"));
+
+        add(&parent1_3, "key", "parent 1 3");
+        assert_eq!(get_key(&env, "key"), make_value("parent 1 3"));
+
+        add(&env, "key", "env");
+        assert_eq!(get_key(&env, "key"), make_value("env"));
+    }
+
+    #[test]
+    fn test_make_env_basic_get_horizontal_parents() {
+        let ground_env = Env::new(vec![]);
+
+        let parent1 = Env::new(vec![ground_env.clone()]);
+        let parent2 = Env::new(vec![ground_env.clone()]);
+        let parent3 = Env::new(vec![ground_env.clone()]);
+
+        let env = Env::new(vec![parent1.clone(), parent2.clone(), parent3.clone()]);
+
+        add(&parent3, "key", "parent3");
+        assert_eq!(get_key(&env, "key"), make_value("parent3"));
+
+        add(&parent2, "key", "parent2");
+        assert_eq!(get_key(&env, "key"), make_value("parent2"));
+
+        add(&parent1, "key", "parent1");
+        assert_eq!(get_key(&env, "key"), make_value("parent1"));
+
+        add(&env, "key", "env");
+        assert_eq!(get_key(&env, "key"), make_value("env"));
+    }
+
+    #[test]
+    fn test_make_env_basic_get_mixed_parents() {
+        let ground_env = Env::new(vec![]);
+
+        let parent1_1 = Env::new(vec![ground_env.clone()]);
+        let parent1_2 = Env::new(vec![parent1_1.clone()]);
+        let parent1_3 = Env::new(vec![parent1_2.clone()]);
+
+        let parent2_1 = Env::new(vec![ground_env.clone()]);
+        let parent2_2 = Env::new(vec![parent2_1.clone()]);
+        let parent2_3 = Env::new(vec![parent2_2.clone()]);
+
+        let parent3_1 = Env::new(vec![ground_env.clone()]);
+        let parent3_2 = Env::new(vec![parent3_1.clone()]);
+        let parent3_3 = Env::new(vec![parent3_2.clone()]);
+
+
+        let env = Env::new(vec![parent1_3.clone(), parent2_3.clone(), parent3_3.clone()]);
+
+        add(&parent3_1, "key", "parent3_1");
+        assert_eq!(get_key(&env, "key"), make_value("parent3_1"));
+
+        add(&parent3_3, "key", "parent3_3");
+        assert_eq!(get_key(&env, "key"), make_value("parent3_3"));
+
+        add(&parent2_2, "key", "parent2_2");
+        assert_eq!(get_key(&env, "key"), make_value("parent2_2"));
+
+        add(&parent1_1, "key", "parent1_1");
+        assert_eq!(get_key(&env, "key"), make_value("parent1_1"));
+
+        add(&env, "key", "env");
+        assert_eq!(get_key(&env, "key"), make_value("env"));
     }
 }
